988a5c45b5577e756fdaf154265c84c71b57ee30
Kaio C. de Oliveira <hello@kaio.dev>
2022-05-21 20:39:54 -0300

    add api client

---
+++ lib/data/core/api_client.dart
@@ -0,0 +1,25 @@
+import 'package:http/http.dart';
+
+import 'api_constants.dart';
+
+class ApiClient {
+  final Client _client;
+
+  ApiClient(this._client);
+
+  dynamic get(String path) async {
+    final response = await _client.get(
+      Uri.parse(
+          '${ApiConstants.BASE_URL}$path?api_key=${ApiConstants.API_KEY}'),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+}
