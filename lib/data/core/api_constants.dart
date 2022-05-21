e3a3c93f2680027d5ede8185fddec8bfe8961fa4
Kaio C. de Oliveira <hello@kaio.dev>
2022-05-21 19:57:10 -0300

    add api constants

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: constant_identifier_names
+
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = String.fromEnvironment('BASE_URL');
+  static const String API_KEY = "3f80fb72251a907226071cafeb8455d0";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
+}
