29b73c7157eeed5121b1bfae744a87930d8e3ba1
Kaio C. de Oliveira <hello@kaio.dev>
2022-05-22 19:23:54 -0300

    add movie repository abstract class with dartz

---
+++ lib/domain/repositories/movie_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies/domain/entities/movie_entity.dart';
+
+import '../entities/app_error.dart';
+
+abstract class MovieRepository {
+  Future<Either<AppError, List<MovieEntity>>> getTrending();
+  Future<Either<AppError, List<MovieEntity>>> getPopular();
+  Future<Either<AppError, List<MovieEntity>>> getPlayingNow();
+  Future<Either<AppError, List<MovieEntity>>> getComingSoon();
+}
