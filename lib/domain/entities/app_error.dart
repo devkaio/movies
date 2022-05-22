e522c24b3f0752ddb256e83599f074d52f3e837f
Kaio C. de Oliveira <hello@kaio.dev>
2022-05-22 19:23:18 -0300

    add apperror class

---
+++ lib/domain/entities/app_error.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class AppError extends Equatable {
+  final String message;
+  const AppError(
+    this.message,
+  );
+
+  @override
+  List<Object?> get props => [message];
+}
