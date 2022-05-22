d4eddbbaade8a91bd7a8d7a2100699650d1f7953
Kaio C. de Oliveira <hello@kaio.dev>
2022-05-22 20:10:37 -0300

    add di instances with package get_it

---
+++ lib/di/get_it.dart
@@ -0,0 +1,37 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:movies/data/core/api_client.dart';
+import 'package:movies/data/data_sources/movie_remote_data_source.dart';
+import 'package:movies/data/repositories/movie_repository_impl.dart';
+import 'package:movies/domain/repositories/movie_repository.dart';
+import 'package:movies/domain/usecases/get_coming_soon.dart';
+import 'package:movies/domain/usecases/get_playing_now.dart';
+import 'package:movies/domain/usecases/get_popular.dart';
+import 'package:movies/domain/usecases/get_trending.dart';
+
+final getItInstance = GetIt.I;
+
+Future init() async {
+  getItInstance.registerLazySingleton<Client>(() => Client());
+
+  getItInstance
+      .registerLazySingleton<ApiClient>(() => ApiClient(getItInstance()));
+
+  getItInstance.registerLazySingleton<MovieRemoteDataSource>(
+      () => MovieRemoteDataSourceImpl(getItInstance()));
+
+  getItInstance
+      .registerLazySingleton<GetTrending>(() => GetTrending(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetComingSoon>(
+      () => GetComingSoon(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetPlayingNow>(
+      () => GetPlayingNow(getItInstance()));
+
+  getItInstance
+      .registerLazySingleton<GetPopular>(() => GetPopular(getItInstance()));
+
+  getItInstance.registerLazySingleton<MovieRepository>(
+      () => MovieRepositoryImpl(getItInstance()));
+}
