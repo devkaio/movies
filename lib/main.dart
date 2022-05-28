df57b4cb461213eefdba514a7d422bd9a68a91ce
Kaio C. de Oliveira <hello@kaio.dev>
2022-05-28 14:17:25 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movies/di/get_it.dart' as get_it;
 
-import 'domain/entities/no_params.dart';
-import 'domain/usecases/get_trending.dart';
+import 'presentation/widgets/movies_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await get_it.init();
 
-  GetTrending getTrending = get_it.getItInstance<GetTrending>();
-
-  final response = await getTrending(NoParams());
-  response.fold(
-    (l) {
-      print("error");
-      print(l);
-    },
-    (r) {
-      print("List of Movies");
-      print(r);
-    },
-  );
-
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(body: Container()),
-    );
-  }
+  runApp(const MoviesApp());
 }
