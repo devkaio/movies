22c3d4c013add7439c12a813ec76f93810516247
Kaio C. de Oliveira <hello@kaio.dev>
2022-06-18 13:21:51 -0300

    bloc: adiciona movie_tabbed_bloc

---
+++ lib/presentation/blocs/movie_tabbed_bloc/movie_tabbed_bloc.dart
@@ -0,0 +1,49 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:movies/domain/entities/app_error.dart';
+import 'package:movies/domain/entities/movie_entity.dart';
+import 'package:movies/domain/usecases/get_coming_soon.dart';
+import 'package:movies/domain/usecases/get_playing_now.dart';
+import 'package:movies/domain/usecases/get_popular.dart';
+
+import '../../../domain/entities/no_params.dart';
+
+part 'movie_tabbed_event.dart';
+part 'movie_tabbed_state.dart';
+
+class MovieTabbedBloc extends Bloc<MovieTabbedEvent, MovieTabbedState> {
+  final GetPopular getPopular;
+  final GetPlayingNow getPlayingNow;
+  final GetComingSoon getComingSoon;
+
+  MovieTabbedBloc({
+    required this.getPopular,
+    required this.getPlayingNow,
+    required this.getComingSoon,
+  }) : super(const MovieTabbedInitial()) {
+    on<MovieTabChangedEvent>((event, emit) async {
+      late Either<AppError, List<MovieEntity>> moviesEither;
+
+      switch (event.currentTabIndex) {
+        case 0:
+          moviesEither = await getPopular(NoParams());
+          break;
+        case 1:
+          moviesEither = await getPlayingNow(NoParams());
+          break;
+        case 2:
+          moviesEither = await getComingSoon(NoParams());
+          break;
+      }
+
+      moviesEither.fold(
+        (l) => emit(MovieTabLoadError(currentTabIndex: event.currentTabIndex)),
+        (r) => emit(MovieTabChanged(
+          movies: r,
+          currentTabIndex: event.currentTabIndex,
+        )),
+      );
+    });
+  }
+}
---
+++ lib/presentation/blocs/movie_tabbed_bloc/movie_tabbed_state.dart
@@ -0,0 +1,31 @@
+part of 'movie_tabbed_bloc.dart';
+
+abstract class MovieTabbedState extends Equatable {
+  final int currentTabIndex;
+
+  const MovieTabbedState({required this.currentTabIndex});
+
+  @override
+  List<Object> get props => [currentTabIndex];
+}
+
+class MovieTabbedInitial extends MovieTabbedState {
+  const MovieTabbedInitial({int currentTabIndex = 0})
+      : super(currentTabIndex: currentTabIndex);
+}
+
+class MovieTabChanged extends MovieTabbedState {
+  final List<MovieEntity> movies;
+
+  const MovieTabChanged({
+    required this.movies,
+    required int currentTabIndex,
+  }) : super(currentTabIndex: currentTabIndex);
+
+  @override
+  List<Object> get props => [currentTabIndex, movies];
+}
+
+class MovieTabLoadError extends MovieTabbedState {
+  const MovieTabLoadError({required super.currentTabIndex});
+}
