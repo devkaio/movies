2533ee8105e65c84925c18e49d601bb8cdc55062
Kaio C. de Oliveira <hello@kaio.dev>
2022-06-19 13:28:14 -0300

    intl: configura traduções

---
+++ lib/common/constants/translation_constants.dart
@@ -0,0 +1,11 @@
+class TranslationConstants {
+  TranslationConstants._();
+
+  static const String favoriteMovie = 'favoriteMovies';
+  static const String language = 'language';
+  static const String about = 'about';
+  static const String feedback = 'feedback';
+  static const String popular = 'popular';
+  static const String now = 'now';
+  static const String soon = 'soon';
+}
