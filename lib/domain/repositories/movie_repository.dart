0c7fd33fb01a3153ca8ceaec07b8a6024444c8b0
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-08 15:30:36 -0300

    update: atualiza MovieRepository

--- lib/domain/repositories/movie_repository.dart
+++ lib/domain/repositories/movie_repository.dart
@@ -1,11 +1,13 @@
 import 'package:dartz/dartz.dart';
-import 'package:movies/domain/entities/movie_entity.dart';
 
 import '../entities/app_error.dart';
+import '../entities/movie_detail_entity.dart';
+import '../entities/movie_entity.dart';
 
 abstract class MovieRepository {
   Future<Either<AppError, List<MovieEntity>>> getTrending();
   Future<Either<AppError, List<MovieEntity>>> getPopular();
   Future<Either<AppError, List<MovieEntity>>> getPlayingNow();
   Future<Either<AppError, List<MovieEntity>>> getComingSoon();
+  Future<Either<AppError, MovieDetailEntity>> getMovieDetail(int id);
 }
